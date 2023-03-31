7c30e8e02d1b02171db2812107024a2175e3765b
Manuel Gutierrez <magutbar@gmail.com>
2023-03-31 13:59:54 +0200

    Primer commit authApp

---
+++ src/app/auth/interfaces/interfaces.ts
@@ -0,0 +1,8 @@
+
+export interface AuthResponse {
+    ok: boolean;
+    uid?: string;
+    name?: string;
+    token?: string;
+    msg?: string;
+}
---
+++ src/app/auth/pages/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styles: [
+  ]
+})
+export class RegisterComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    name: [ 'Test 1', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ 'test1@test.com', [ Validators.required, Validators.email ] ],
+    password: [ '123456', [ Validators.required, Validators.minLength(6) ] ]
+  });
+
+  constructor( private fb: FormBuilder,
+                private router: Router ) { }
+
+  registro() {
+    console.log(this.miFormulario.value);
+
+    this.router.navigateByUrl('/dashboard');
+  }
+
+}
